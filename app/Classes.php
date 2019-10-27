52a26202937b6bf42cd6ad848a84e14ffd25f763
Muhammad Shakirin Samin <cybersharky94@gmail.com>
2019-10-27 20:18:39 +0800

    Testing Audit Trails process @ Classes model

--- app/Classes.php
+++ app/Classes.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Classes extends Model
+class Classes extends Model implements Auditable
 {
+    use \OwenIt\Auditing\Auditable;
+
     //
     protected $fillable = [
         'class_name',
